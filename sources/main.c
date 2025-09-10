7c92cda0e8d751fe561e9e35744735d2d46bf87e
Thomas Jacquelinet <tjacquel@f6r11s9.clusters.42paris.fr>
2025-09-10 13:37:58 +0200

    modif makefile

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: tjacquel <tjacquel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/09 12:27:42 by pbret             #+#    #+#             */
-/*   Updated: 2025/09/10 12:28:34 by tjacquel         ###   ########.fr       */
+/*   Updated: 2025/09/10 12:34:18 by tjacquel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,9 @@
 int	main(int argc, char **argv)
 {
 	(void)argv;
-	if (argc == 2)
-	{
-		printf("valid\n");
-	}
-	else
-		printf("invalid\n");
+	if (argc != 2)
+		return (printf("invalid\n"), 1);
+	printf("valid\n");
+	check_file(argv[1]);
 	return (0);
 }
-
-test
