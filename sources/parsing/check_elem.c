91f5b3d155b0e204f3ee84b3f471286a1a5c2f7a
Thomas Jacquelinet <Thomas@MacBook-Pro-8.local>
2025-09-13 00:27:39 +0200

    travail en cours jai commence -- mon train arrive

--- sources/parsing/check_elem.c
+++ sources/parsing/check_elem.c
@@ -25,33 +25,119 @@ static void	check_rest_of_line(char **line)
 	}
 }
 
-//static void color_getter(t_data *data, char **line, int key_id)
-//{
-//	//unsigned int		n;
-
-//	//printf("color_getter line = `%s`", *line);
-
-//	//n = 0;
-//	//while (*line[n] && *line[n] != ' ' && *line[n] != '\n')
-//	//	n++;
-//	//printf("n = %d\n", n);
-//	//data->elem.path[key_id] = ft_strndup(*line, n);
-
-//	//*line += n;
-
-//	//printf("data->elem.path[%d] = %s", key_id, data->elem.path[key_id]);
-
-//}
-
-
-static void path_getter(t_data *data, char **line, int key_id)
+static void	floor_color(t_data *data, char **line)
 {
+	unsigned int	n;
+	char			*temp_line;
+	// char			*temp_color;
+	int				i;
+
+	i = 0;
+	while (i < 3)
+	{
+		temp_line = *line;
+		n = 0;
+		while (**line && **line != ',' && **line != '\n') // attention le comportement me parait bizarre quand plsrs ',' par exemple F 250,,225,40
+		{
+			if (!ft_isdigit(**line) && **line != ' ') // si le char n'est pas un espace ou pas un chiffre
+			{
+				printf ("je sors parce que jai trouve le char `%c`\n", **line);
+				exit (1);
+			}
+			(*line)++;
+			n++;
+		}
+		// temp_color = ft_strndup(temp_line, n);
+		// printf ("temp_color = %s\n", temp_color);
+		// int color = ft_atoi(temp_color);
+		// printf ("color = %d\n", color);
+
+		// if (color < 0)
+		// 	exit (1);
+		if (data->elem.f_value[i] == -1)
+			data->elem.f_value[i] = ft_atoi(ft_strndup(temp_line, n)); // ne permet pas de verifier si le malloc de ft_strndup fail ou si ft_atoi renvoie une valeur de retour erreur
+		else
+		{
+			printf ("doublon floor couleur\n");
+			exit(1);
+		}
+		if (**line == ',' && i !=2) // je veux etre certain de pas skip un ',' en fin de RGB par exemple F 250,225,175,
+			(*line)++;
+		i++;
+	}
+}
+
+static void	ceiling_color(t_data *data, char **line)
+{
+	unsigned int	n;
+	char			*temp_line;
+	char			*temp_color;
+	int				i;
+
+	i = 0;
+
+	while (i < 3)
+	{
+		temp_line = *line;
+		n = 0;
+		while (**line && **line != ',' && **line != '\n')
+		{
+			if (!ft_isdigit(**line) && **line != ' ')
+			{
+				printf ("je sors parce que jai trouve le char `%c`\n", **line);
+				exit (1);
+			}
+			(*line)++;
+			n++;
+		}
+		temp_color = ft_strndup(temp_line, n);
+		printf ("temp_color = %s\n", temp_color);
+		int color = ft_atoi(temp_color);
+		printf ("color = %d\n", color);
+
+		if (color < 0)
+			exit (1);
+		if (data->elem.c_value[i] == -1)
+			data->elem.c_value[i] = color;
+		else
+		{
+			printf ("doublon ceiling couleur\n");
+			exit(1);
+		}
+		if (**line == ',' && i !=2)
+			(*line)++;
+		// else
+		// 	exit (1);
+
+		i++;
+	}
+}
+
+static void	color_getter(t_data *data, char **line, t_key id_key)
+{
+	// faudrait essayer de faire differemment je pense parce que virtuellement les fonctions
+	// floor_color() et ceiling_color() sont les memes fonctions
+	if (id_key == F)
+		floor_color(data, line);
+	else if (id_key == C)
+		ceiling_color(data, line);
+	else
+	{
+		printf("exit color_getter()\n");
+
+		exit (1);
+	}
+}
+
+
+static void path_getter(t_data *data, char **line, t_key id_key)
+{
+	printf("path_getter line = `%s`", *line);
+
 	unsigned int	n;
 	char*			temp_line;
 
-	printf("path_getter line = `%s`", *line);
 	temp_line = *line;
-
 	n = 0;
 	while (**line && **line != ' ' && **line != '\n')
 	{
@@ -60,10 +146,16 @@ static void path_getter(t_data *data, char **line, int key_id)
 	}
 	printf("n = %d\n", n);
 
-	data->elem.path[key_id] = ft_strndup(temp_line, n);
+	if (!data->elem.path[id_key]) // protection pour eviter les doublons
+		data->elem.path[id_key] = ft_strndup(temp_line, n);
+	else
+	{
+		printf("doublon path\n");
+		exit(1);
+	}
 
 
-	printf("data->elem.path[%d] = %s\n", key_id, data->elem.path[key_id]);
+	printf("data->elem.path[%d] = %s\n", id_key, data->elem.path[id_key]);
 
 }
 
@@ -103,15 +195,20 @@ static void	check_line(t_data *data, char *line)
 				line++;
 			if (id_key < F)
 				path_getter(data, &line, id_key);
-			//else
-			//	color_getter(data, &line, id_key);
+			else
+				color_getter(data, &line, id_key);
 			printf("rest of the line after getter function : `%s`", line);
 			check_rest_of_line(&line);
 			return ;
 		}
 		id_key++;
 	}
-	exit (1);
+	if (data->elem.e_counter < 6)
+	{
+		printf ("pas assez d'elements pour initialiser la map check_line()\n");
+		exit (1);
+	}
+	// exit (1); // sans le if statement au dessus, on exit forcement si ya pas assez d'element pour initialiser la carte
 }
 
 void	check_elem(t_data *data, char *file_map)
@@ -130,19 +227,20 @@ void	check_elem(t_data *data, char *file_map)
 		check_line(data, line);
 		free(line);
 		if (data->elem.e_counter == 6)
-		{
-			while ((line = get_next_line(fd)))
-				free(line);
 			break ;
-		}
 	}
+	printf ("\n\n----------------------- Elem values after data->elem.e_counter == 6 ---------------------\n");
+	print_elem(&data->elem);
 	if (data->elem.e_counter < 6)
+	{
+		printf ("pas assez d'elements pour initialiser la map check_elem()\n");
 		exit (1);
+	}
 	// check_path(data);
 	// check_color(data);
 }
 
-// TO DO - 
+// TO DO -
 // COLOR_GETTER
 // LIGNES VIDES (*avec ou sans espaces)
 // check le path (check_path) - sil est valide, sil existe, si les droits sont bons etc.
